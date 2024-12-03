107ec9688fec60d0afddbf9ff4094fc213092798
BorisKornev <borik621@gmail.com>
2024-12-03 15:35:50 +0300

    Add files via upload

---
+++ module_2_6.py
@@ -0,0 +1,9 @@
+first = input ('Введите первое число:')
+second = input ('Введите второе число:')
+third = input ('Введите третье число:')
+if first == second and first == third:
+    print ("Совпадений:", 3)
+elif first == second or first == third:
+    print("Совпадений:", 2)
+else:
+    print("Совпадений:", 0)
